724d9b3a4f29e478df2ab3018d3f6670ccb9a771
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2022-10-22 18:52:12 +0100

    Updated the current time
    Week day, date, hours/minutes.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,43 @@
+//calculate the current date from dt
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  //fixing single-digit bug
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  //fixing single-digit bug
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  //converting numbers to actual week days
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "87471c6a89e6a023ff918939ca16d5ac";
