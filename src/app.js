f761e1d6559ea034579442afb95855da97d31c2c
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2022-11-10 15:51:35 +0000

    Injected the HTML into JS
    Created a new function displayForecast, selected the created element, created a new string with the class of row to create a grid. Created a new variable with days and looped through each day, and inside it we're overriding the forecastHTML by adding the content before and concatenating the block of HTML.

--- src/app.js
+++ src/app.js
@@ -25,6 +25,37 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+//function for the forecast
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+              <div class="weather-forecast-date">
+              ${day}
+              </div>
+              <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" 
+              width="40"
+              />
+              <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-max-temperature">
+              18º</span>
+              <span class="weather-forecast-min-temperature">
+                12º</span>
+              </div>
+            </div>
+              `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //functions changing live info
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -101,3 +132,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelciusTemperature);
 
 search("New York");
+
+displayForecast();
