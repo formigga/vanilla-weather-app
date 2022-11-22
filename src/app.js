713d30e3b505e5cbb860c551dd31d91967256853
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2022-11-22 19:31:41 +0000

    Added the API for the forecast

--- src/app.js
+++ src/app.js
@@ -26,7 +26,8 @@ function formatDate(timestamp) {
 }
 
 //function for the forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -56,6 +57,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+//function getting the coords from data
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "ad793a6d772939c31783de5822791acf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 //functions changing live info
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -79,6 +87,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 //makes an ajax call and displays the city
@@ -132,5 +142,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelciusTemperature);
 
 search("New York");
-
-displayForecast();
