d50dc4d1f40a6dc246390f1dc1ed9eae0395c602
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2022-11-22 21:01:37 +0000

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -25,32 +25,46 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+//getting the day of the week
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 //function for the forecast
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
+  console.log(response.data.daily);
   let forecastHTML = `<div class="row">`;
-  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="col-2">
               <div class="weather-forecast-date">
-              ${day}
+              ${formatDay(forecastDay.dt)}
               </div>
-              <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" 
+              <img src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" alt="" 
               width="40"
               />
               <div class="weather-forecast-temperatures">
               <span class="weather-forecast-max-temperature">
-              18º</span>
+              ${Math.round(forecastDay.temp.max)}º</span>
               <span class="weather-forecast-min-temperature">
-                12º</span>
+                ${Math.round(forecastDay.temp.min)}º</span>
               </div>
             </div>
               `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
